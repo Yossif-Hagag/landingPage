1ce89f2430cc33e0b19eeccaf2476d44078254cb
yossif hagag <yousefhagag179@google.com>
2020-10-29 23:22:05 +0200

    solve, complete and resubmit the project

--- js/app.js
+++ js/app.js
@@ -48,8 +48,13 @@ for (let i = 1; i <= sections.length; i++) {
 let addClassActive = function(input){
     for (let i = 0; i < sections.length; i++) {
         sections[i].classList.remove('your-active-class');
+        links[i].style.backgroundColor = '';
     };
-    sections[input].classList.add('your-active-class');
+    if (input != undefined) {
+    	sections[input].classList.add('your-active-class');
+    	sections[input].scrollIntoView({behavior : 'smooth'});
+    	links[input].style.backgroundColor = 'orange';
+    }
 }
 
 // Scroll to anchor ID using scrollTO event
@@ -65,16 +70,24 @@ let addClassActive = function(input){
 
 // Scroll to section on link click
 links[0].addEventListener('click', function () {
-links[0].href = "#section " + 1;});
+	links[0].href = "#section " + 1;
+	addClassActive(0);
+});
 
 links[1].addEventListener('click', function () {
-links[1].href = "#section " + 2;});
+	links[1].href = "#section " + 2;
+	addClassActive(1);
+});
 
 links[2].addEventListener('click', function () {
-links[2].href = "#section " + 3;});
+	links[2].href = "#section " + 3;
+	addClassActive(2);
+});
 
 links[3].addEventListener('click', function () {
-links[3].href = "#section " + 4;});
+	links[3].href = "#section " + 4;
+	addClassActive(3);
+});
 // Set sections as active
 
 
